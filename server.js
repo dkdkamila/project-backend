d0bfff0e1ead44a3321915bd1a60a8905eae48b9
dkdkamila <dominika.kamila.berg@gmail.com>
2025-01-28 18:56:30 +0100

    connect

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/recommendations', recommendationRoutes);
 
 // MongoDB Connection
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
